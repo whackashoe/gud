8ec3a3a6d6da0a1ab45ed1a5fd9a5d863df6c8cb
Jett <whackashoe@gmail.com>
2015-03-15 14:50:18 -0500

    json example

--- examples/helloworld/main.cpp
+++ examples/helloworld/main.cpp
@@ -12,6 +12,25 @@ int main(int argc, char * argv[])
 			res.stream() << "Hello world!";
 		});
 
+		app.get("/json", [](web::request & req, web::response & res) {
+			web::json json = {
+				{ "pi", 3.141 },
+				{ "happy", true },
+				{ "name", "Niels" },
+				{ "nothing", nullptr },
+				{ "answer", {
+					{ "everything", 42 }
+				}},
+				{ "list", { 1, 0, 2 } },
+				{ "object", {
+					{ "currency", "USD" },
+					{ "value", 42.99 }
+				}}
+			};
+
+			res.stream() << json;
+		});
+
 		std::cout << "web server started on 127.0.0.1:" << port << std::endl;
 		app.listen(port);
 	} catch (std::exception const & e) {
