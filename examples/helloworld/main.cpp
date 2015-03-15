1f830f2700d8897a121552da1f8947ef084af883
Jett <whackashoe@gmail.com>
2015-03-15 15:24:36 -0500

    controller example

--- examples/helloworld/main.cpp
+++ examples/helloworld/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <web/web.hpp>
 
+struct ApiController
+{
+	static void miscdata(web::request & req, web::response & res)
+	{
+		web::json json = {
+			{ "pi", 3.141 },
+			{ "happy", true },
+			{ "name", "Niels" },
+			{ "nothing", nullptr },
+			{ "answer", {
+				{ "everything", 42 }
+			}},
+			{ "list", { 1, 0, 2 } },
+			{ "object", {
+				{ "currency", "USD" },
+				{ "value", 42.99 }
+			}}
+		};
+
+		res.stream() << json;
+	}
+};
+
 
 int main(int argc, char * argv[])
 {
@@ -8,28 +31,13 @@ int main(int argc, char * argv[])
 	constexpr int port = 3333;
 
 	try {
+		// You can use lambda expressions
 		app.get("/", [](web::request & req, web::response & res) {
 			res.stream() << "Hello world!";
 		});
 
-		app.get("/json", [](web::request & req, web::response & res) {
-			web::json json = {
-				{ "pi", 3.141 },
-				{ "happy", true },
-				{ "name", "Niels" },
-				{ "nothing", nullptr },
-				{ "answer", {
-					{ "everything", 42 }
-				}},
-				{ "list", { 1, 0, 2 } },
-				{ "object", {
-					{ "currency", "USD" },
-					{ "value", 42.99 }
-				}}
-			};
-
-			res.stream() << json;
-		});
+		// Or simply reference a publicstatic method in a controller
+		app.get("/json", ApiController::miscdata);
 
 		std::cout << "web server started on 127.0.0.1:" << port << std::endl;
 		app.listen(port);
